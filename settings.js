cc4a64ccd14531545e71f81c4fb94b10c5731b47
SatanTech <140904777+SatanTech@users.noreply.github.com>
2023-09-20 22:19:43 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.creator = 'SatanTech'// yourname
+global.MONGO_DB_URI = "database mongodb" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "-@satantech@($752@)" //isi apa aja bebas
+global.your_email = "bangtepllo752@gmail.com" //email
+global.email_password = "tepllo752" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
