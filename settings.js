a901dcfd8d42e7220cd65aa84773271e38705e79
SatanTech <140904777+SatanTech@users.noreply.github.com>
2023-10-18 07:38:52 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,27 +1,27 @@
 const fs = require('fs')
 
-global.creator = 'SatanTech'// yourname
-global.MONGO_DB_URI = "database mongodb" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@satantech@($752@)" //isi apa aja bebas
-global.your_email = "bangtepllo752@gmail.com" //email
-global.email_password = "tepllo752" //application password email
+global.creator = 'AdminSF'// yourname
+global.MONGO_DB_URI = "mongodb+srv://sfvpnteam752:1tMKf3H2oPcz0ChN@cluster0.l5btbqh.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "SatanFusion" //isi apa aja bebas
+global.your_email = "sfvpnteam752@gmail.com" //email
+global.email_password = "vcaavlvzzhefquyl" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
 	noapikey:{
 		status: 403,
-        message: 'Input parameter apikey',
+        message: 'apikeynya mana deck!',
         creator: `${creator}`,
         result: "error"
     },
     error: {
         status: 503,
-        message: 'Service Unavaible, Sedang dalam perbaikan',
+        message: 'Service tidak tersedia, Sedang dalam perbaikan',
         creator: `${creator}`
     },
     apikey: {
     	status: 403,
-    	message: 'Forbiden, Invalid apikey',
+    	message: 'Terlarang, Invalid apikey',
     	creator: `${creator}`
     },
     noturl: {
